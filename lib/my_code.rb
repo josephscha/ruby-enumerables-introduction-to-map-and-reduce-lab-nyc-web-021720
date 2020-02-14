395f8e8a61dab557f3fe611df95df27375c48127
Joseph S. Cha <josephshincha@gmail.com>
2020-02-14 00:15:53 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,11 @@
 # My Code here....
+def map_to_negativize(array)
+  negativized_array = []
+  index = 0 
+  while index < array.length do 
+    negativized_array << (array[index] * -1)
+  end 
+  index += 1 
+end 
+  negativized_array
+end
