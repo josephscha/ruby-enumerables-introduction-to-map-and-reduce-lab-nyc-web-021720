50ec36da7ab3abdcc968eec26f102f17ad1f2d01
Joseph S. Cha <josephshincha@gmail.com>
2020-02-14 00:20:53 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -12,3 +12,13 @@ end
 def map_to_no_change(array)
   array 
 end
+
+def map_to_double(array)
+  doubled_array = []
+  index = 0 
+  while index < array.length do 
+    doubled_array << (array[index] * 2)
+  index += 1 
+end
+doubled_array 
+end
